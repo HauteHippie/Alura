983ddad7b94ea0f6a89f4b392146b9a4182791ac
João Vinicius Yuji Nakano <103541244+HauteHippie@users.noreply.github.com>
2022-04-27 20:17:43 -0300

    Update imagens.js

--- JavaScript/freeway-JavaScript/imagens.js
+++ JavaScript/freeway-JavaScript/imagens.js
@@ -10,5 +10,5 @@ function preload() {
   carro1 = loadImage("imagens/carro-1.png");
   carro2 = loadImage("imagens/carro-2.png");
   carro3 = loadImage("imagens/carro-3.png");
-  carros = [carro1, carro2, carro3];
+  carros = [carro1, carro2, carro3, carro3, carro1, carro2];
 }
