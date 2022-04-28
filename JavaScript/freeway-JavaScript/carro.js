e05f3817b92a1e655ef39374c2d6827ae1c1f517
João Vinicius Yuji Nakano <103541244+HauteHippie@users.noreply.github.com>
2022-04-28 19:10:00 -0300

    Update carro.js

--- JavaScript/freeway-JavaScript/carro.js
+++ JavaScript/freeway-JavaScript/carro.js
@@ -1,13 +1,11 @@
 // Variáveis do carro
-let yCarros = [42, 98, 150, 210, 270, 318];
-let xCarros = [600, 600, 600, 700, 800, 600];
-let velocidadeCarros = [2, 3.5, 4, 3, 1, 2];
-let larguraCarro = 66;
-let comprimentoCarro = 36;
+let yCarros = [42, 98, 150];
+let xCarros = [600, 600, 600];
+let velocidadeCarros = [2, 3.5, 4];
 
 function mostraCarro() {
   for (let i = 0; i < carros.length; i++) {
-    image(carros[i], xCarros[i], yCarros[i], larguraCarro, comprimentoCarro);
+    image(carros[i], xCarros[i], yCarros[i], 66, 36);
   }
 }
 
