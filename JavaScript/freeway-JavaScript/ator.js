c022069678abe16bcacbbd0dd2ec179531271175
João Vinicius Yuji Nakano <103541244+HauteHippie@users.noreply.github.com>
2022-04-28 19:42:15 -0300

    Update ator.js

--- JavaScript/freeway-JavaScript/ator.js
+++ JavaScript/freeway-JavaScript/ator.js
@@ -1,6 +1,7 @@
 // Variáveis do ator
 let xAtor = 277;
 let yAtor = 366;
+let hit = false;
 
 let pontos = 0;
 
@@ -10,11 +11,30 @@ function mostraAtor() {
 
 function movimentaAtor() {
   if (keyIsDown(UP_ARROW)) {
-    yAtor -= 4;
+      yAtor -= 4; 
   }
   else if (keyIsDown(DOWN_ARROW)) {
+    if (podeMover()) {
     yAtor += 4;
+    }
   }
+}
+
+function verificaColisao() {
+  for (let i = 0; i < carros.length; i++) {
+    hit = collideRectCircle(xCarros[i], yCarros[i], larguraCarro, comprimentoCarro, xAtor, yAtor, 15);
+    if (hit) {
+      colidiu();
+      somColisao.play();
+      if ( pontosMaiorQueZero()) {
+        pontos--; 
+      }
+    }
+  }
+}
+
+function colidiu() {
+  yAtor = 366;
 }
 
 function incluiPontos(){
@@ -26,7 +46,16 @@ function incluiPontos(){
 
 function marcaPontos() {
   if (yAtor < 30) {
+    somPonto.play();
     pontos++;   
     yAtor = 366;
   }
+}
+
+function pontosMaiorQueZero() {
+  return pontos > 0;
+}
+
+function podeMover() {
+  return yAtor < 366;
 }
