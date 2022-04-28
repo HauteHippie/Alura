7ef674bdca1d4116183223b1ad7b0054119f2f9e
João Vinicius Yuji Nakano <103541244+HauteHippie@users.noreply.github.com>
2022-04-28 19:44:28 -0300

    Update imagens.js

--- JavaScript/freeway-JavaScript/imagens.js
+++ JavaScript/freeway-JavaScript/imagens.js
@@ -1,8 +1,13 @@
-// Carregando imagens
+// Carregando imagens e sons do jogo
+
 let imagemEstrada;
 let imagemAtor;
 let imagemCarro;
 let carros;
+
+let somTrilha;
+let somColisao;
+let somPonto;
 
 function preload() {
   imagemEstrada = loadImage("imagens/estrada.png");
@@ -10,5 +15,9 @@ function preload() {
   carro1 = loadImage("imagens/carro-1.png");
   carro2 = loadImage("imagens/carro-2.png");
   carro3 = loadImage("imagens/carro-3.png");
-  carros = [carro1, carro2, carro3, carro3, carro1, carro2];
+  carros = [carro1, carro2, carro3, carro1, carro3, carro2];
+  
+  somTrilha = loadSound("sons/trilha.mp3");
+  somColisao = loadSound("sons/colidiu.mp3");
+  somPonto = loadSound("sons/pontos.wav");
 }
