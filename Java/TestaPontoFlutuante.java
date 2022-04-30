172a59d7ffa29dff1ed37c25f324e26013d48df7
João Vinicius Yuji Nakano <103541244+HauteHippie@users.noreply.github.com>
2022-04-30 13:20:46 -0300

    Add files via upload

---
+++ Java/TestaPontoFlutuante.java
@@ -0,0 +1,15 @@
+public class TestaPontoFlutuante {
+    public static void main(String[] args) {
+        double salario = 1250.70 ;
+        System.out.println("O seu salário é: " + salario);
+
+        int divisao = 5 / 2;                // O resultado esperado será 2
+        System.out.println("O resultado da divisão será: " + divisao);
+
+        double divisaoDois = 5 / 2;         // O resultado esperado será 2.0
+        System.out.println("O resultado da divisão será: " + divisaoDois);
+
+        double divisaoTres = 5.0 / 2;       // O resultado esperado será 2.5
+        System.out.println("O resultado da divisão será: " + divisaoTres);
+    }
+}
